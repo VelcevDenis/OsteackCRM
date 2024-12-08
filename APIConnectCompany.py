74a2ed2db88dbe07766f91ee2a4580005863baad
Denis Velcev <denis.v@t-teck.com>
2024-12-08 14:33:23 +0200

    make table sortable

--- APIConnectCompany.py
+++ APIConnectCompany.py
@@ -17,14 +17,15 @@ router = APIRouter(
 user_dependency = Annotated[dict, Depends(metodAuth.get_current_user)]
 
 class CreateConnectCompanyRequest(BaseModel):  
-    company_id: int | None       
+    company_id: int | None = None
     firm_name: str  
     email: str
     phone: str | None  
     next_meeting: datetime | None
     is_approved: bool
     status: columns.StatusEnum = columns.StatusEnum.pending  
-    description:str | None    
+    description: str | None
+    last_update: datetime | None = None
 
 class EditConnectCompanyRequest(BaseModel): 
     next_meeting: datetime | None
@@ -42,28 +43,31 @@ def get_db():
 db_dependency = Annotated[Session, Depends(get_db)]
 
 @router.post("/recallCompanis/add", status_code=status.HTTP_201_CREATED)
-async def create_connectCompany(u: user_dependency, db: db_dependency, connectCompany: CreateConnectCompanyRequest):    
+async def create_connectCompany(u: user_dependency, db: db_dependency, connectCompany: CreateConnectCompanyRequest):  
     create_company_model = columns.Companis(
-        firm_name= connectCompany.firm_name,  
-        email= connectCompany.email,
-        phone= connectCompany.phone,
-        created_at= datetime.utcnow() 
+        firm_name=connectCompany.firm_name,  
+        email=connectCompany.email,
+        phone=connectCompany.phone,
+        created_at=datetime.utcnow()
     )            
     db.add(create_company_model)
     db.commit()
+    db.refresh(create_company_model)
     
     create_connect_company_model = columns.ConnectCompanis(
-        worker_id = u.get('id'),
-        company_id = create_company_model.id,
-        created_at = datetime.utcnow(),
-        next_meeting = connectCompany.next_meeting,
-        is_approved = 0,
-        status = connectCompany.status,
-        description = connectCompany.description
+        worker_id=u.get('id'),
+        company_id=create_company_model.id,
+        created_at=datetime.utcnow(),
+        next_meeting=connectCompany.next_meeting,
+        is_approved=0,
+        status=connectCompany.status,
+        description=connectCompany.description
     )    
 
     db.add(create_connect_company_model)
     db.commit()
+    return {"message": "Company and connection created successfully."}
+
 
 @router.get("/recallCompanis/by-pt-id{connectCompany_id}", status_code=status.HTTP_200_OK)
 async def read_connectCompany(u: user_dependency, connectCompany_id:int, db: db_dependency):  
@@ -73,9 +77,7 @@ async def read_connectCompany(u: user_dependency, connectCompany_id:int, db: db_
     return connectCompany
 
 @router.get("/recallCompanis/by-worker-id/{worker_id}", status_code=status.HTTP_200_OK, response_model=List[column_models.ConnectCompanyBase])
-async def read_connect_company_by_name(u: user_dependency, db: db_dependency, worker_id: int) -> List[column_models.ConnectCompanyBase]:    
-    # if worker_id == 10000000:
-    #     companis = db.query(columns.ConnectCompanis).all()
+async def read_connect_company_by_name(u: user_dependency, db: db_dependency, worker_id: int) -> List[column_models.ConnectCompanyBase]:
     if worker_id:
         companis = db.query(columns.ConnectCompanis).filter(columns.ConnectCompanis.worker_id == worker_id).all()
 
@@ -83,7 +85,7 @@ async def read_connect_company_by_name(u: user_dependency, db: db_dependency, wo
 
 
 @router.get("/potentialCompanies/all", response_model=List[CreateConnectCompanyRequest])
-async def list_of_all_potential_companis(u: user_dependency, db: db_dependency, skip: int = 0, limit: int = 100):
+async def list_of_all_potential_companis(u: user_dependency, db: db_dependency, skip: int = 0, limit: int = 5):
     potential_clients = db.query(columns.ConnectCompanis).join(
         columns.Companis, columns.ConnectCompanis.company_id == columns.Companis.id
     ).filter(columns.ConnectCompanis.worker_id == u.get('id')).offset(skip).limit(limit).all()
@@ -97,7 +99,8 @@ async def list_of_all_potential_companis(u: user_dependency, db: db_dependency, 
             next_meeting=client.next_meeting,
             is_approved=client.is_approved,
             status=client.status,
-            description=client.description
+            description=client.description,
+            last_update=client.last_update
         )
         for client in potential_clients
     ]
